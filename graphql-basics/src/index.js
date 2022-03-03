1dccd824d8c14ce8779ced75ed4db2d159bc4753
Robin Lee <robin@theathletic.com>
2022-03-04 00:23:18 +1100

    First commit

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,44 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Scalar type - String, Boolean, Int, Float, ID
+
+// Type definition (Schema)
+const typeDefs = `
+  type Query {
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+  }
+`
+
+// Resolvers
+const resolvers = {
+  Query: {
+    title() {
+      return 'Book 1'
+    },
+    price() {
+      return 10.8
+    },
+    releaseYear() {
+      return null
+    },
+    rating() {
+      return 4.5
+    },
+    inStock() {
+      return true
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => {
+  console.log('The server is up!')
+})
